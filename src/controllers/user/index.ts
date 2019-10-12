f32ba731371c985ae341b42fb513bdffcd5c2b78
Lukas Bacevicius <lukas.bacevicius@valtech.com>
2019-10-12 19:09:43 +0200

    feat(User): Adds a login route

--- src/controllers/user/index.ts
+++ src/controllers/user/index.ts
@@ -8,19 +8,43 @@ export default class UserController extends GenericController {
         super(fastify, model);
 
         this.create = this.create.bind(this);
+        this.login = this.login.bind(this);
     }
 
 
     async create(request, reply) {
         try {
-            const { email, password } = request.body;
+            const { password } = request.body;
 
             request.body.password = await this.fastify.bcrypt.hash(password, 10);
 
             const result = await this.model.create(request.body);
-            
+
             return reply.code(201).send(result);
         } catch (error) {
+            request.log.error(error);
+            return reply.code(400).send(error);
+        }
+    }
+
+    async login(request, reply) {
+        try {
+            const { password, email } = await this.model.findOne({ email: request.body.email });
+            return this.fastify.bcrypt.compare(
+                request.body.password,
+                password,
+                (error, valid) => {
+                    if (valid) {
+                        return reply.code(201).send({
+                            token: 'Basic ' + Buffer.from(`${email}:${password}`).toString('base64')
+                        })
+                    }
+
+                    return reply.send(this.fastify.httpErrors.unauthorized('Email or password is incorrect'));
+                }
+            );
+        }
+        catch (error) {
             request.log.error(error);
             return reply.code(400).send(error);
         }
--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -4,9 +4,10 @@ import UserController from '../../controllers/user';
 export default fp(async (server, opts, next) => {
     const User = new UserController(server, server.db.models.User);
 
+    server.post('/login', User.login);
+    server.post('/user', {}, User.create);
     server.get('/user/:_id', {}, User.findOne);
     server.patch('/user/:_id', {}, User.update);
-    server.post('/user', {}, User.create);
 
     next();
 });
